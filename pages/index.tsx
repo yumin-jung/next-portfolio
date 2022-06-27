7a9d987f4068ecd4c61fed96d9abcf0267b430ed
yumin-jung <jungym887@naver.com>
2022-06-27 17:41:24 +0900

    Apply tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ const Home: NextPage = () => {
         <meta name="description" content="portfolio by yumin" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline">
+        Home
+      </h1>
     </Layout>
   )
 }
