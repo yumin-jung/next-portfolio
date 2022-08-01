0f8d15d486a1b717c4a21e062ab590a41beb120b
yumin-jung <jungym887@naver.com>
2022-08-01 16:02:45 +0900

    Fix a tag target error

--- components/header.tsx
+++ components/header.tsx
@@ -8,18 +8,34 @@ const Header = () => {
                 <div className="container mx-auto flex flex-wrap p-5 px-10 flex-col md:flex-row items-center">
                     <Link href="/">
                         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                            <span className="ml-2 mr-2 text-xl">Portfolio</span>
+                            <span className="ml-2 mr-2 text-xl">
+                                Portfolio
+                            </span>
                         </a>
                     </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                         <Link href="/">
-                            <a className="ml-2 mr-2 hover:text-gray-900">Home</a>
+                            <a className="ml-2 mr-2 hover:text-gray-900">
+                                Home
+                            </a>
                         </Link>
                         <Link href="/projects">
-                            <a className="ml-2 mr-2 hover:text-gray-900">Project</a>
+                            <a className="ml-2 mr-2 hover:text-gray-900">
+                                Project
+                            </a>
                         </Link>
-                        <a href="https://github.com/yumin-jung" target="_blank" className="ml-2 mr-2 hover:text-gray-900">GitHub</a>
-                        <a href="https://medium.com/@yumin-jung" target="_blank" className="ml-2 mr-2 hover:text-gray-900">Blog</a>
+                        <a href="https://github.com/yumin-jung"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="ml-2 mr-2 hover:text-gray-900">
+                            GitHub
+                        </a>
+                        <a href="https://medium.com/@yumin-jung"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="ml-2 mr-2 hover:text-gray-900">
+                            Blog
+                        </a>
                     </nav>
                     <DarkModeToggleButton />
                 </div>
