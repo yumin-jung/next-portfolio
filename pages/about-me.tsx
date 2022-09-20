1c8c058c14e8648c7c7e799d78bc9d2ef710ac0a
yumin-jung <jungym887@naver.com>
2022-09-20 21:13:32 +0900

    Update about page

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Image from 'next/image'
+import profileImage from '../public/profile.jpeg'
 
 const About: NextPage = () => {
   return (
@@ -12,8 +14,20 @@ const About: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <div className="flex">
-          <div>About Me</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 sm:mx-16">
+          <div className="w-2/3 m-auto rounded-full overflow-hidden">
+            <Image
+              src={profileImage}
+              layout="responsive"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center sm:items-start py-10 sm:py-4 sm:px-10">
+            <div className="text-sm sm:text-lg">Yumin Jung</div>
+            <div className="text-sm my-2 text-gray-800">Interested in FE / UX / Optimization. </div>
+            <div className="text-2xl sm:text-7xl mb-16">Less is more.</div>
+          </div>
         </div>
       </div>
     </Layout>
