da75215b7f5b2dc4cf26a19ea936ebcf48e48cfd
yumin-jung <jungym887@naver.com>
2023-01-12 22:48:13 +0900

    Fix: css 관련 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   mode: 'jit',
   content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
-      extend: {},
+    extend: {},
   },
   plugins: [],
 }
