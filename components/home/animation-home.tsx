522ad174db599081f729ca441fdcab992a5bc56b
Yumin Jung <jungym887@naver.com>
2023-01-23 13:07:40 +0900

    feat: Add location change (#87)

--- components/home/animation-home.tsx
+++ components/home/animation-home.tsx
@@ -8,6 +8,11 @@ const AnimationHome: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     let pointerInteracting: number | null = null;
     let pointerInteractionMovement = 0;
+    const locationToAngles = (lat: number, long: number) => {
+        return [Math.PI - ((long * Math.PI) / 180 - Math.PI / 2), (lat * Math.PI) / 180]
+    }
+    const focusRef = useRef([0, 0])
+    let isClicked = false;
 
     const [{ r }, api] = useSpring(() => ({
         r: 0,
@@ -20,7 +25,6 @@ const AnimationHome: React.FC = () => {
     }));
 
     useEffect(() => {
-        let phi = 0;
         const { current: canvas } = canvasRef;
 
         if (!canvas) return;
@@ -65,10 +69,14 @@ const AnimationHome: React.FC = () => {
         if (!canvasRef.current) return;
 
         let width = 0;
+        let phi = 0;
+        let currentPhi = 0;
+        let currentTheta = 0;
+        const doublePi = Math.PI * 2;
         const onResize = () => canvasRef.current && (width = canvasRef.current.offsetWidth)
         window.addEventListener('resize', onResize)
         onResize()
-        createGlobe(canvasRef.current, {
+        const globe = createGlobe(canvasRef.current, {
             devicePixelRatio: 2,
             width: width * 2,
             height: width * 2,
@@ -87,18 +95,47 @@ const AnimationHome: React.FC = () => {
                 { location: [36.3504119, 127.3845475], size: 0.03 }
             ],
             onRender: (state) => {
-                // Called on every animation frame.
-                // `state` will be an empty object, return updated params.
-                state.phi = phi + r.get()
-                phi += 0.005
+                const [focusPhi, focusTheta] = focusRef.current
+                const distPositive = (focusPhi - currentPhi + doublePi) % doublePi
+                const distNegative = (currentPhi - focusPhi + doublePi) % doublePi
+                // Control the speed
+                if (distPositive < distNegative) {
+                    currentPhi += distPositive * 0.08
+                } else {
+                    currentPhi -= distNegative * 0.08
+                }
+                currentTheta = currentTheta * 0.92 + focusTheta * 0.08
+                if (isClicked) {
+                    state.phi = currentPhi
+                    state.theta = currentTheta
+                    setTimeout(() => {
+                        isClicked = false
+                        focusRef.current = locationToAngles(0, 0)
+                        phi = currentPhi
+                    }, 1000);
+                } else {
+                    state.phi = phi + r.get()
+                    state.theta = 0
+                    phi += 0.005
+                }
                 state.width = width * 2
                 state.height = width * 2
             }
         });
+        return () => globe.destroy()
     }, []);
 
     return (
-        <canvas ref={canvasRef} className="w-full h-full aspect-square cursor-grab transition-opacity" />
+        <>
+            <div className="flex flex-col md:flex-row justify-center items-center control-buttons" style={{ gap: '.5rem' }}>
+                I live in
+                <button className="underline" onClick={() => {
+                    isClicked = true
+                    focusRef.current = locationToAngles(36.3504119, 127.3845475)
+                }}>Daejeon</button>
+            </div>
+            <canvas ref={canvasRef} className="w-full h-full aspect-square cursor-grab transition-opacity" />
+        </>
     )
 }
 
