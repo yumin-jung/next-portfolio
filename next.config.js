de70fcb18c0379c3837fb49d43f70ac7a216db10
yumin-jung <jungym887@naver.com>
2023-04-05 03:02:53 +0900

    Fix: Add url in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     domains: [
       'www.notion.so',
       'images.unsplash.com',
-      's3.us-west-2.amazonaws.com'
+      's3.us-west-2.amazonaws.com',
+      'https%3A%2F%2Fs3.us-west-2.amazonaws.com'
     ],
     formats: ['image/avif', 'image/webp']
   }
