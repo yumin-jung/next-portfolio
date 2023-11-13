5b9c46fac64c5303ba6d5f963dc8fc09fff69886
Yumin Jung <jungym887@naver.com>
2023-11-13 17:59:33 +0900

    Update portfolio (#138)
    * feat: Add design portfolio
    
    * feat: Update portfolio and resume
    
    * feat: Update portfolio

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -31,7 +31,7 @@ const About: NextPage = () => {
             <div className="text-xl sm:text-3xl mb-6 mt-10">
               Yumin Jung&nbsp;
               <Link className="text-base underline"
-                href={"./portfolio-231109.pdf"}
+                href={"./portfolio-231113.pdf"}
                 target="_blank"
                 rel="noopener noreferrer"
                 locale={false}
